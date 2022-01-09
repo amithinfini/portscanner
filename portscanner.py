8f61035e2af3c1e61048cfae286971a32e0a2c60
amithinfini <amith.infini@gmail.com>
2022-01-09 17:13:25 -0500

    added banner grabbing

--- portscanner.py
+++ portscanner.py
@@ -4,7 +4,7 @@ from IPy import IP  #for converting domains into IP's
 def scan(target):
     converted_ip = check_ip(target)
     print('\n[-_0] Scanning Target ' + str(target))
-    for port in range(79, 89):
+    for port in range(1,100):
         scan_port(converted_ip, port)
 
 def check_ip(ip):
@@ -14,12 +14,19 @@ def check_ip(ip):
     except ValueError:
         return socket.gethostbyname(ip)
 
+def get_banner(s):
+    return s.recv(1024)
+
 def scan_port(ipaddress, port):
     try:
         sock = socket.socket()
         sock.settimeout(0.5)  # to make the scanning faster
         sock.connect((ipaddress, port))
-        print('[+] Port ' + str(port) + ' is Open')
+        try:
+            banner = get_banner(sock)
+            print('[+] Open port ' + str(port) + ' : ' + str(banner.decode().strip('\n')))
+        except:
+            print('[+] Open port ' + str(port))
     except:
         pass
 
